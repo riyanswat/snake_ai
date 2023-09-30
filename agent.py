366aa59576631e87680be6c59d96f13239b29211
riyanswat <riyanswat@gmail.com>
2023-10-01 00:25:21 +0500

    updated

--- agent.py
+++ agent.py
@@ -13,3 +13,26 @@ LR = 0.001
 class Agent:
     def __init__(self):
         pass
+
+    def get_state(self, game):
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
+        pass
+
+
+def train():
+    pass
+
+
+if __name__ == '__main__':
+    train()
